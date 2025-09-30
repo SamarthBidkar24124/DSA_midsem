6e193e98b6a8adff2418cebfa077aad1cac019c0
AadiSB <aaditya.bhand@gmail.com>
2025-09-30 08:23:02 +0530

    Create Aaditya_contribution.c

---
+++ Aaditya_contribution.c
@@ -0,0 +1,25 @@
+SRTF (Preemptive SJF)
+void srtf(Process p[], int n) {
+    int time = 0, done = 0;
+    for (int i = 0; i < n; i++) p[i].remaining = p[i].burst;
+
+    while (done < n) {
+        int idx = -1, minRT = 1e9;
+        for (int i = 0; i < n; i++) {
+            if (p[i].arrival <= time && p[i].remaining > 0 && p[i].remaining < minRT) {
+                minRT = p[i].remaining;
+                idx = i;
+            }
+        }
+        if (idx == -1) { time++; continue; }
+
+        p[idx].remaining--;
+        time++;
+        if (p[idx].remaining == 0) {
+            done++;
+            p[idx].turnaround = time - p[idx].arrival;
+            p[idx].waiting = p[idx].turnaround - p[idx].burst;
+        }
+    }
+    printResults(p, n);
+}
